f6d8b05a128d4ecd0ab103cebd89441de18da7c8
schulya <96183796+Schuyaa@users.noreply.github.com>
2022-11-03 20:28:21 +0500

    Delete js directory

--- js/parallax_2.js
+++
@@ -1,5 +0,0 @@
-document.querySelectorAll('.cursor').forEach(node => {
-    document.addEventListener('mousemove', e => {
-        node.style.cssText = `--move-x: ${e.clientX}px; --move-y: ${e.clientY}px;`
-    })
-})
